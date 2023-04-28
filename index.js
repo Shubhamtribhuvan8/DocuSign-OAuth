5c55499b8d3a0394e5b9be41c1bb3bba718b53aa
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-04-29 00:27:42 +0530

    dotenv+apiclinet

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require("express");
 const bodyParser = require('body-parser');
 const path=require("path");
 const dotenv=require("dotenv");
+const docusign=require("docusign-esign")
+const fs=require("fs")
 const app=express();
 dotenv.config();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +13,11 @@ console.log("recived form data",request.body);
 response.send("Recived")
 })
 
-app.get("/",(request,response)=>{
+app.get("/",async(request,response)=>{
+    let dsApiClient = new docusign.ApiClient();
+    dsApiClient.setBasePath(process.env.BASE_PATH);
+    const results = await dsApi.requestJWTUserToken(process.env.INTEGRATION_KEY,process.env.USER_ID ,"signature", fs.readFileSync(path.join(__dirname,"private_key")), 3600);
+    
     response.sendFile(path.join(__dirname,"main.html"))
 })
 
