a05a1cdbcc301fbd9f8daebbf6a7ee77e4f21b4a
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-04-29 23:04:56 +0530

    react-mui-roboto

--- client/src/Compo/DocumentSign.jsx
+++ client/src/Compo/DocumentSign.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Button, Input } from '@mui/material';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { ToastContainer, toast } from 'react-toastify';
+//npm i react-toastify
 function Form() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -20,44 +25,49 @@ function Form() {
       console.log(error)
     }
   };
-
+  const commonStyles = {
+    bgcolor: 'background.paper',
+    margin: "auto",
+    top:1,
+    borderColor: 'text.primary',
+    width: '29rem',
+    height: '17rem',
+  };
   return (
-    <>
-      <h1>Docusign</h1>
+      <div>
+      <ToastContainer/>
+     <h1>Docusign</h1>
+      <Box sx={{ ...commonStyles, border: 1 }}>
+        <br/>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name : </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-         <br />
-         <br />
-        <label htmlFor="email">Email : </label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        <br />
-        <br />
-        <label htmlFor="company">Company : </label>
-        <input
-          type="text"
+        <TextField required type="text" 
+        id="name"
+        name="name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+         placeholder="Name" />
+      <br /> <br /> 
+
+        <TextField required  type="text"
+        id="email"
+        name="email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+           placeholder="Email" />
+      <br /> <br />  
+
+      <TextField required  type="text"
           id="company"
           name="company"
           value={company}
           onChange={(event) => setCompany(event.target.value)}
-        />
-         <br />
-         <br />
-        <button type="submit">Submit</button>
-      </form>
-    </>
+           placeholder="Company" />
+      <br /> <br />  
+
+      <Button variant="outlined" type="submit"> Submit</Button>
+     </form>
+       </Box>
+    </div>
   );
 }
 
